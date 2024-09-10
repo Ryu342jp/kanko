e430e8cce251ce5cd8f01797f7a23ec2fb27ef10
Ryu342jp <134501131+Ryu342jp@users.noreply.github.com>
2024-09-10 16:28:23 +0900

    Update shopScript.js

--- project/Map/shopScript.js
+++ project/Map/shopScript.js
@@ -1,1 +1,11 @@
 
+const backgroundImages = [];
+const imgElements = document.querySelectorAll('.section img');
+
+imgElements.forEach(img => {
+    backgroundImages.push(img.src);
+});
+
+let currentIndex = 0;
+
+
