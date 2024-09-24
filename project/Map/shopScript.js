6c01de30eff49f9351a68798a319a000f7ccf82b
Ryu342jp <134501131+Ryu342jp@users.noreply.github.com>
2024-09-24 14:08:00 +0900

    Add files via upload

---
+++ project/Map/shopScript.js
@@ -0,0 +1,10 @@
+// ナビゲーションの設定
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navbarMenu = document.querySelector(".navbar-menu");
+
+    menuToggle.addEventListener("click", function () {
+        navbarMenu.classList.toggle("active");
+        menuToggle.classList.toggle("active");
+    });
+});
